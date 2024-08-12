d7cf678273559fd76dc288ea647fba4a261bd684
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-08-12 13:40:30 +0530

    feat : add notification modal , get suggested users in backend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
 export const getUserProfile = async (req, res) => {
@@ -22,23 +23,62 @@ export const followUnfollowUser = async (req, res) => {
 
         if( !userToModify || !currentUser) return res.status(400).json({ error: "User not found" });
 
-        const isFollowing = currentUser.followers.includes(currentUser._id);
+        const isFollowing = currentUser.following.includes(id);
+        console.log(isFollowing);
+        
 
         if(isFollowing) {
-            await User.findByIdAndUpdate(id,{$pull: {followers: req.user._id}});
-            await User.findByIdAndUpdate(req.user._id,{$pull: {following: id}});
-            res.status(200).json({message: "User unfollowed"});
+          await User.findByIdAndUpdate(id, {
+            $pull: { followers: req.user._id },
+          });
+          await User.findByIdAndUpdate(req.user._id, {
+            $pull: { following: id },
+          });
+          //TODO return the id of the user as a response
+          res.status(200).json({ message: "User unfollowed" });
         }
         else{
             await User.findByIdAndUpdate(id,{$push: {followers: req.user._id}});  
             await User.findByIdAndUpdate(req.user._id,{$push: {following: id}});
-            res.status(200).json({message: "User followed"});
+            
             //send notification
+            const newNotification = new Notification({
+              type: "follow",
+              from: req.user._id,
+              to: userToModify._id
+            })
+            await newNotification.save();
+            //TODO return the id of the user as a response
+            res.status(200).json({message: "User followed successfully"});
         }
-
+        
     } catch (error) {
         console.log("Error in followUnfollowUser controller:", error.message);
         
         res.status(500).json({ error: "Internal Server Error" });
     }
+};
+
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const usersFollowedByMe = await User.findById(userId).select("following");
+    const users = await User.aggregate([
+      {
+        $match: {_id:{$ne:userId}},
+      },
+      {
+        $sample: { size: 10 },
+      }
+    ])
+    const filteredUsers = users.filter((user) => !usersFollowedByMe.following.includes(user._id))
+    const suggestedUsers =filteredUsers.slice(0,4)
+    suggestedUsers.forEach((user)=> user.password = null)
+res.status(200).json(suggestedUsers)
+  } catch (error) {
+    console.log("Error in getSuggestedUsers controller:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+    
+  }
 };
---
+++ backend/models/notification.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+const notficationSchema = new mongoose.Schema({
+    from:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    to:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    type:{
+        type:String,
+        required:true  ,
+        enum:['like','follow']
+    },
+    read:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps:true})
+const Notification = mongoose.model('Notification',notficationSchema)
+
+export default Notification
