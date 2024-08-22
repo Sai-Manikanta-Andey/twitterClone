e22c30f23dc93e80b445a64deba1da4df5c5b0b8
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-08-22 10:40:26 +0530

    chore : backend complete

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 dotenv.config();
 cloudinary.config({
@@ -30,6 +31,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notification", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
