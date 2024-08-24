df58363dc1511a7f8147e4b1e7de408df3a45b1c
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-08-24 14:58:02 +0530

    feat: frontend connected to backend & Signin,Login,Logout functionality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,76 @@
-import { Routes, Route } from "react-router-dom";
-import HomePage from './pages/home/HomePage'
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import Sidebar from "./components/common/Sidebar";
-import RightPanel from './components/common/RightPanel'
+import RightPanel from "./components/common/RightPanel";
 import NotificationPage from "./pages/notification/Notification";
 import ProfilePage from "./pages/profile/ProfilePage";
+import { Toaster } from "react-hot-toast";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
 function App() {
+  const queryClient = useQueryClient();
+  const {
+    data: authUser,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me", {});
+        const data = await res.json();
+
+        if (res.status === 401) return null;
+        if (!res.ok) throw new Error(data.error || "Failed to get user");
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false,
+  });
+  console.log(authUser);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex max-w-6xl mx-auto">
       {/* common component */}
-      <Sidebar/>
+      { authUser && <Sidebar />}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifications" element={<NotificationPage />} />
-        <Route path="/profile/:username" element={<ProfilePage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUpPage />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <LoginPage />}
+        />
+        <Route
+          path="/notifications"
+          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile/:username"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
-      <RightPanel/>
+      {authUser && <RightPanel />}
+      <Toaster />
     </div>
   );
 }
 
-
-export default App
+export default App;
